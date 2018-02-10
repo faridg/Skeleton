03a0a07913f917eac5709e131886cae1bc47d6cd
faridg <faridg@twc.com>
2018-02-10 03:52:23 -0500

    menu test
    basic page with menu items

--- index.php
+++ index.php
@@ -35,12 +35,27 @@
   <div class="container">
     <div class="row">
       <div class="one-half column" style="margin-top: 25%">
-        <h4>Basic Page</h4>
-        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+        <h4>Skeletheme Basic Page</h4>
+        <p>This index.php page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
       </div>
     </div>
   </div>
+	
+	<div class="navbar-spacer"></div>
+	   <nav class="navbar">
+		  <div class="container">
+			<ul class="navbar-list">
+			  <li class="navbar-item"><a class="navbar-link" href="#intro">Home</a></li>
+			  <li class="navbar-item"><a class="navbar-link" href="#about">About</a></li>
+			  <li class="navbar-item"><a class="navbar-link" href="#services">Services</a></li>
+			  <li class="navbar-item"><a class="navbar-link" href="#contact">Contact</a></li>
+			</ul>
+		  </div>
+		</nav>
 
+  
+  
+  
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
