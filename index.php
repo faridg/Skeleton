0b31a7f1140b74aa32df9d1b63c9cd86954eb9c0
faridg <faridg@twc.com>
2018-02-10 19:08:10 -0500

    indent

--- index.php
+++ index.php
@@ -2,61 +2,61 @@
 <html lang="en">
 <head>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?></title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+<!-- Basic Page Needs
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php bloginfo('name'); ?></title>
+<meta name="description" content="">
+<meta name="author" content="">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Mobile Specific Metas
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+<!-- FONT
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-  
-  <!-- Scripts
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<!-- CSS
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+<!-- Scripts
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
 
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
+<!-- Favicon
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<link rel="icon" type="image/png" href="images/favicon.png">
 
 </head>
 
 <body <?php body_class(); ?>>
 
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-	<div class="navbar-spacer"></div>
-	<nav class="nav main-nav">
-		<div class="container">
-			<?php
-				$args = array(
-					'theme_location' => 'primary',
-				);
-			?>
-			<?php wp_nav_menu($args); ?>
-		</div>
-	</nav>
-		<div class="container">
-		<div class="row">
-		  <div class="one-half column" style="margin-top: 25%">
-			<h4>Skeletheme Basic Page</h4>
-			<p>This index.php page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
-		  </div>
-		</div>
-		</div>
-	<?php wp_footer(); ?>
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="navbar-spacer"></div>
+<nav class="nav main-nav">
+<div class="container">
+	<?php
+		$args = array(
+			'theme_location' => 'primary',
+		);
+	?>
+	<?php wp_nav_menu($args); ?>
+</div>
+</nav>
+<div class="container">
+<div class="row">
+  <div class="one-half column" style="margin-top: 25%">
+	<h4>Skeletheme Basic Page</h4>
+	<p>This index.php page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
+  </div>
+</div>
+</div>
+<?php wp_footer(); ?>
 <!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 </html>
